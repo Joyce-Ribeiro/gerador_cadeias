9d8e37050343eb328425c0cc00bbc14da5112357
Joyce-Ribeiro <58228924+Joyce-Ribeiro@users.noreply.github.com>
2024-10-12 23:46:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ print('Bem-vindo ao Gerador de Cadeias para Gramáticas Livres de Contexto')
 # Solicitar ao usuário o nome do arquivo da gramática
 nome_arquivo = input("Digite o nome do arquivo da gramática (ex: gramatica.txt): ")
 gramatica = ler_arquivo_gramatica(nome_arquivo)
+res = []
 
 # Exibir a gramática lida
 if gramatica:
@@ -15,15 +16,19 @@ if gramatica:
 
     while True:
         # Solicitar ao usuário o número máximo de bits
-        tamanho = int(input("Digite o número de maximo bits para a cadeias: "))
-        cadeias_formadas = [] # Cadeias_formadas finais formadas
+        tamanho = int(input("Digite o número máximo de bits para as cadeias: "))
+        cadeias_formadas = []  # Cadeias formadas finais
+
         # Gerar cadeias com o tamanho especificado
-        resultado, producoes = forma_cadeias(gramatica, tamanho=tamanho, cadeias_formadas= cadeias_formadas)
+        resultado, producoes = forma_cadeias(gramatica, tamanho=tamanho, cadeias_formadas=cadeias_formadas)
 
         # Exibir as cadeias formadas com suas produções
         print("Cadeias formadas com suas produções:")
         for cadeia, p in zip(resultado, producoes):
-            print(cadeia, "->", p)
+            # Verificar se a cadeia e produção já foram registradas
+            if [cadeia, p] not in res:
+                print(cadeia, "->", p)
+                res.append([cadeia, p])
 
         # Perguntar ao usuário se deseja gerar uma nova cadeia
         continuar = input("Deseja gerar uma nova cadeia? (s/n): ").strip().lower()
