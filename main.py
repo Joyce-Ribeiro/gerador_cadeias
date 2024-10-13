7914a4b32cdb9cacf9a8fda20f8256697a8d239d
Joyce-Ribeiro <58228924+Joyce-Ribeiro@users.noreply.github.com>
2024-10-12 23:23:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,25 +8,26 @@ nome_arquivo = input("Digite o nome do arquivo da gramática (ex: gramatica.txt)
 gramatica = ler_arquivo_gramatica(nome_arquivo)
 
 # Exibir a gramática lida
-print("Gramática lida:")
-for chave, valor in gramatica.items():
-    print(f"{chave}: {valor}")
+if gramatica:
+    print("Gramática lida:")
+    for chave, valor in gramatica.items():
+        print(f"{chave}: {valor}")
 
-while True:
-    # Solicitar ao usuário o número máximo de bits
-    tamanho = int(input("Digite o número de bits para a cadeias: "))
-    cadeias_formadas = [] # Cadeias_formadas finais formadas
-    # Gerar cadeias com o tamanho especificado
-    resultado, producoes = forma_cadeias(gramatica, tamanho=tamanho, cadeias_formadas= cadeias_formadas)
+    while True:
+        # Solicitar ao usuário o número máximo de bits
+        tamanho = int(input("Digite o número de maximo bits para a cadeias: "))
+        cadeias_formadas = [] # Cadeias_formadas finais formadas
+        # Gerar cadeias com o tamanho especificado
+        resultado, producoes = forma_cadeias(gramatica, tamanho=tamanho, cadeias_formadas= cadeias_formadas)
 
-    # Exibir as cadeias formadas com suas produções
-    print("Cadeias formadas com suas produções:")
-    for cadeia, p in zip(resultado, producoes):
-        print(cadeia, "->", p)
+        # Exibir as cadeias formadas com suas produções
+        print("Cadeias formadas com suas produções:")
+        for cadeia, p in zip(resultado, producoes):
+            print(cadeia, "->", p)
 
-    # Perguntar ao usuário se deseja gerar uma nova cadeia
-    continuar = input("Deseja gerar uma nova cadeia? (s/n): ").strip().lower()
-    if continuar != 's':
-        break
+        # Perguntar ao usuário se deseja gerar uma nova cadeia
+        continuar = input("Deseja gerar uma nova cadeia? (s/n): ").strip().lower()
+        if continuar != 's':
+            break
 
-print("Obrigado por usar o Gerador de Cadeias!")
+    print("Obrigado por usar o Gerador de Cadeias!")
